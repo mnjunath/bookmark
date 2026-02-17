df1c9689d079bc635f1e6d943c46a03093a2be39
mnjunath <mnjunathrdy@gmail.com>
2026-02-17 19:25:37 +0530

    Ultra-robust OAuth redirect detection using Host header and VERCEL_URL

--- utils/get-url.ts
+++ utils/get-url.ts
@@ -1,8 +1,10 @@
 export const getURL = () => {
     let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set on Vercel deployment.
-        'http://localhost:3000/'
+        process?.env?.NEXT_PUBLIC_SITE_URL ??
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+        process?.env?.VERCEL_URL ?? // Non-prefixed version available in server-side
+        'localhost:3000'
+
     // Make sure to include `https://` when not localhost.
     url = url.includes('http') ? url : `https://${url}`
     // Make sure to include a trailing `/`.
